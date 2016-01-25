c8556725c2926623fe07b8e6d6da976604255131
joamag <joamag@hive.pt>
2016-01-25 14:21:08 +0000

    better code

--- src/cameria/static/js/main.js
+++ src/cameria/static/js/main.js
@@ -90,56 +90,50 @@
             // width and height of the camera needs update it's updated
             // in accordance with the current size
             matchedObject.load(function() {
-                        // retrieves the current context as the camera component
-                        // and uses it to retrieve the currently set width and
-                        // height to be verified
-                        var camera = jQuery(this);
-                        var cameraWidth = camera.data("width");
-                        var cameraHeight = camera.data("height");
+                // retrieves the current context as the camera component
+                // and uses it to retrieve the currently set width and
+                // height to be verified
+                var camera = jQuery(this);
+                var cameraWidth = camera.data("width");
+                var cameraHeight = camera.data("height");
 
-                        // verifies if the current values are valid and in
-                        // case they're not updates them with the current
-                        // visual sizes then updates the values in the camera
-                        // data structure
-                        cameraWidth = cameraWidth > 0
-                                ? cameraWidth
-                                : camera.outerWidth();
-                        cameraHeight = cameraHeight > 0
-                                ? cameraHeight
-                                : camera.outerHeight();
-                        camera.data("width", cameraWidth);
-                        camera.data("height", cameraHeight);
-                    });
+                // verifies if the current values are valid and in
+                // case they're not updates them with the current
+                // visual sizes then updates the values in the camera
+                // data structure
+                cameraWidth = cameraWidth > 0 ? cameraWidth : camera.outerWidth();
+                cameraHeight = cameraHeight > 0 ? cameraHeight : camera.outerHeight();
+                camera.data("width", cameraWidth);
+                camera.data("height", cameraHeight);
+            });
 
             // registers for the key down on the window so that
             // we can handle the fullscreen trigger, note that once
             // the current object is destroyed the event is
             // unregistered to avoid double registration
-            matchedObject.length
-                    && _window.keydown(onKeyDown = function(event) {
-                        // retrieves the key value
-                        var keyValue = event.keyCode
-                                ? event.keyCode
-                                : event.charCode ? event.charCode : event.which;
+            matchedObject.length && _window.keydown(onKeyDown = function(event) {
+                // retrieves the key value
+                var keyValue = event.keyCode ? event.keyCode : event.charCode ? event.charCode :
+                    event.which;
 
-                        if (event.shiftKey && keyValue == 70) {
-                            var maximized = camera.data("maximized") || false;
-                            if (maximized) {
-                                _minimize(camera);
-                                _window.unbind("resize", __maximize)
-                                _window.unbind("scroll", __fixedScroll);
-                                camera.data("maximized", false);
-                            } else {
-                                _maximize(camera);
-                                _window.resize(__maximize);
-                                _window.scroll(__fixedScroll);
-                                camera.data("maximized", true);
-                            }
-                        }
-                    });
+                if (event.shiftKey && keyValue == 70) {
+                    var maximized = camera.data("maximized") || false;
+                    if (maximized) {
+                        _minimize(camera);
+                        _window.unbind("resize", __maximize)
+                        _window.unbind("scroll", __fixedScroll);
+                        camera.data("maximized", false);
+                    } else {
+                        _maximize(camera);
+                        _window.resize(__maximize);
+                        _window.scroll(__fixedScroll);
+                        camera.data("maximized", true);
+                    }
+                }
+            });
             matchedObject.bind("destroyed", function() {
-                        _window.unbind("keydown", onKeyDown);
-                    });
+                _window.unbind("keydown", onKeyDown);
+            });
         };
 
         /**
@@ -344,41 +338,40 @@
             // timeout that has passed (in case no connection
             // is available no refresh is done)
             interval = setInterval(function() {
-                        _update();
-                    }, timeout);
+                _update();
+            }, timeout);
             _body.data("interval", interval);
         };
 
         /**
          * Registers the event handlers for the created objects.
          */
-        var _registerHandlers = function() {
-        };
+        var _registerHandlers = function() {};
 
         var _update = function() {
             // runs the remote request on the current document url
             // to make sure the connection is available before refresh
             jQuery.ajax({
-                        url : document.URL,
-                        success : function(data, status) {
-                            // in case no data is retrieved, the server is
-                            // considered to be down and (returns immediately)
-                            if (!data) {
-                                return;
-                            }
+                url: document.URL,
+                success: function(data, status) {
+                    // in case no data is retrieved, the server is
+                    // considered to be down and (returns immediately)
+                    if (!data) {
+                        return;
+                    }
 
-                            // in case the status of the received message is not the
-                            // correct one (success or not modified) the message is
-                            // considered invalid (returns immediately)
-                            if (status != "success" && status != "notmodified") {
-                                return;
-                            }
+                    // in case the status of the received message is not the
+                    // correct one (success or not modified) the message is
+                    // considered invalid (returns immediately)
+                    if (status != "success" && status != "notmodified") {
+                        return;
+                    }
 
-                            // reloads the current location to provide the interface
-                            // with the must uptodate data
-                            location.reload(true);
-                        }
-                    });
+                    // reloads the current location to provide the interface
+                    // with the must uptodate data
+                    location.reload(true);
+                }
+            });
         };
 
         // initializes the plugin
@@ -405,8 +398,8 @@
 })(jQuery);
 
 jQuery(document).ready(function() {
-            var _body = jQuery("body");
-            _body.bind("applied", function(event, base) {
-                        base.uapply();
-                    });
-        });
+    var _body = jQuery("body");
+    _body.bind("applied", function(event, base) {
+        base.uapply();
+    });
+});
